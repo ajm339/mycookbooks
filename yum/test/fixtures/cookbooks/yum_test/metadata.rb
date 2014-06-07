6c17f784fe8b4764284dd780d6de2ce1c40943e1
Alex Meyers <ajm339@cornell.edu>
2014-06-07 10:41:50 -0700

    replaced yum with a newer version

--- yum/test/fixtures/cookbooks/yum_test/metadata.rb
+++ yum/test/fixtures/cookbooks/yum_test/metadata.rb
@@ -1,13 +1,9 @@
-name 'yum'
+name 'yum_test'
 maintainer 'Chef'
 maintainer_email 'cookbooks@getchef.com'
 license 'Apache 2.0'
 description 'Configures various yum components on Red Hat-like systems'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '3.2.1'
+version '2.4.3'
 
-supports 'redhat'
-supports 'centos'
-supports 'scientific'
-supports 'amazon'
-supports 'fedora'
+depends 'yum'
