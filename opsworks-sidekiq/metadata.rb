89a9814abaf50eae9ff1c8639e56602282fb0933
Alex Meyers <ajm339@cornell.edu>
2014-07-19 09:30:22 -0700

    newrelic and python cookbooks

--- opsworks-sidekiq/metadata.rb
+++
@@ -1,4 +0,0 @@
-name        "sidekiq"
-description "Sidekiq receipes for Opsworks Ubuntu"
-maintainer  "Brainy Atom"
-version     "1.0.0"
--- redis-config/metadata.rb
+++
@@ -1,7 +0,0 @@
-name             'redis-config'
-maintainer       'Daapr'
-maintainer_email 'Daapr'
-license          'All rights reserved'
-description      'Installs/Configures redis-config'
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
